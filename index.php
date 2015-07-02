d48bda7951eb1fc98d58258392d5edb2d144aca3
rellbows <ryan.ellis927@gmail.com>
2015-07-02 00:31:28 -0500

    added get parameter for lastname

--- index.php
+++ index.php
@@ -1,10 +1,16 @@
 <html>
-<head>
-<title>Hello World</title>
-</head>
-<body>
-<h1>Hello <?php 
- if(isset($_GET['name'])) { echo $_GET['name'];} else { echo "ADD NAME TO QUERY PARAMS!!!!";}
-?>!</h1>
-</body>
+	<head>
+		<title>Hello World</title>
+	</head>
+	<body>
+		<h1>Hello 
+			<?php 
+				if(isset($_GET['name']) && isset($_GET['lastname'])) { 
+					echo $_GET['name'] . " " . $_GET['lastname'];
+				} else { 
+					echo "ADD NAME TO QUERY PARAMS!!!!";
+				}
+			?>!
+		</h1>
+	</body>
 </html>
