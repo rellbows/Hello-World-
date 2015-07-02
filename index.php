bc840f6b76370be56211bd0268a18472b87212c2
rellbows <ryan.ellis927@gmail.com>
2015-07-02 00:39:19 -0500

    changed error message as suggested in PR

--- index.php
+++ index.php
@@ -3,14 +3,14 @@
 		<title>Hello World</title>
 	</head>
 	<body>
-		<h1>Hello 
+		<h1>
 			<?php 
 				if(isset($_GET['name']) && isset($_GET['lastname'])) { 
-					echo $_GET['name'] . " " . $_GET['lastname'];
+					echo "Hello" . $_GET['name'] . " " . $_GET['lastname'] . "!";
 				} else { 
-					echo "ADD NAME TO QUERY PARAMS!!!!";
+					echo "<span style='color:red;'>NO NAMES DECLARED! WHO ARE YOU?</span>";
 				}
-			?>!
+			?>
 		</h1>
 	</body>
 </html>
