95814f21170c815fa28086078f68b2383d021ae7
rellbows <ryan.ellis927@gmail.com>
2015-07-08 00:03:59 -0500

    form for name params created and revised index file

---
+++ NameGame.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Name Game</title>
+	</head>
+	
+	<body>
+		<h1>Please enter you name below...</h1>
+			<form action="index.php" method="get">
+					First Name: <input type="text" name="name" /><br />
+					Last Name: <input type="text" name="lastname" /><br />
+					<input type="submit" />
+			</form>
+	</body>
+</html>
--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<body>
 		<h1>
 			<?php 
-				if(isset($_GET['name']) && isset($_GET['lastname'])) { 
+				if(isset($_GET['name']) && !empty($_GET['name']) && isset($_GET['lastname']) && !empty($_GET['lastname'])) { 
 					echo "Hello " . $_GET['name'] . " " . $_GET['lastname'] . "!";
 				} else { 
 					echo "<span style='color:red;'>NO NAMES DECLARED! WHO ARE YOU?</span>";
